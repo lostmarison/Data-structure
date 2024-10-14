3b46385f05834b2717a4e951ec5ed770c50b6a4b
lostmarison <146553120+lostmarison@users.noreply.github.com>
2024-10-14 12:26:47 +0800

    Update BiTree1.cpp

--- Tree/BiTree.cpp
+++ Tree/BiTree.cpp
@@ -1,35 +1,34 @@
+#include <deque>
 #include <iostream>
 using namespace std;
 
-/*
-为了避免调用者访问BiTree类的私有成员root，在构造函数、析构函数以及遍历函数中调用了相应的私有函数
-例如：在函数PreOrder()中调用了私有函数PreOrder(root)
-*/
-
-template <typename T> struct TreeNode {
+template <typename T>
+struct TreeNode {
     T data;
-    TreeNode<T>*lchid, *rchild;
+    TreeNode<T>* left;
+    TreeNode<T>* right;
+    TreeNode(T x) : data(x), left(nullptr), right(nullptr) {}
 };
 
-template <typename T> 
+template <typename T>
 class BiTree {
-public:
-    BiTree() { root = Creat(); }          // 建立二叉树
-    ~BiTree() { Release(root); }          // 释放各结点存储空间
-    void PreOrder() { PreOrder(root); }   // 前序遍历二叉树
-    void InOrder() { InOrder(root); }     // 中序遍历二叉树
-    void PostOrder() { PostOrder(root); } // 后序遍历二叉树
-    // void LevelOrder();//层序遍历二叉树
-private:
-    TreeNode<T>* Creat();            // 构造含函数调用
-    void Release(TreeNode<T>* bt);   // 析构函数的调用
-    TreeNode<T>* root;               // 指向根结点的头指针
-    void PreOrder(TreeNode<T>* bt);  // 前序遍历函数调用
-    void InOrder(TreeNode<T>* bt);   // 中序遍历函数调用
-    void PostOrder(TreeNode<T>* bt); // 后序遍历函数调用
+   public:
+    BiTree() { root = Creat(); }           // 建立二叉树
+    ~BiTree() { Release(root); }           // 释放各结点存储空间
+    void PreOrder() { PreOrder(root); }    // 前序遍历二叉树
+    void InOrder() { InOrder(root); }      // 中序遍历二叉树
+    void PostOrder() { PostOrder(root); }  // 后序遍历二叉树
+    void LevelOrder();                     // 层序遍历二叉树
+   private:
+    TreeNode<T>* Creat();             // 构造含函数调用
+    void Release(TreeNode<T>* bt);    // 析构函数的调用
+    TreeNode<T>* root;                // 指向根结点的头指针
+    void PreOrder(TreeNode<T>* bt);   // 前序遍历函数调用
+    void InOrder(TreeNode<T>* bt);    // 中序遍历函数调用
+    void PostOrder(TreeNode<T>* bt);  // 后序遍历函数调用
 };
 
-template <typename T> 
+template <typename T>
 TreeNode<T>* BiTree<T>::Creat() {
     TreeNode<T>* bt;
     char ch;
@@ -39,73 +38,74 @@ TreeNode<T>* BiTree<T>::Creat() {
     } else {
         bt = new TreeNode<T>;
         bt->data = ch;
-        bt->lchid = Creat();  // 递归建立左子树
-        bt->rchild = Creat(); // 递归建立右子树
+        bt->left = Creat();   // 递归建立左子树
+        bt->right = Creat();  // 递归建立右子树
     }
     return bt;
 }
 
-template <typename T> 
+template <typename T>
 void BiTree<T>::Release(TreeNode<T>* bt) {
     if (bt == nullptr) {
         return;
     } else {
-        Release(bt->lchid);
-        Release(bt->rchild);
+        Release(bt->left);
+        Release(bt->right);
         delete bt;
     }
 }
 
-template <typename T> 
+template <typename T>
 void BiTree<T>::PreOrder(TreeNode<T>* bt) {
     if (bt == nullptr) {
         return;
     } else {
-        cout << bt->data << "\t"; // 访问根结点的数据域
-        PreOrder(bt->lchid);      // 前序递归遍历bt的左子树
-        PreOrder(bt->rchild);     // 前序递归遍历bt的右子树
+        cout << bt->data << "\t";  // 访问根结点的数据域
+        PreOrder(bt->left);        // 前序递归遍历bt的左子树
+        PreOrder(bt->right);       // 前序递归遍历bt的右子树
     }
 }
 
-template <typename T> 
+template <typename T>
 void BiTree<T>::InOrder(TreeNode<T>* bt) {
     if (bt == nullptr) {
         return;
     } else {
-        InOrder(bt->lchid);       // 中序递归遍历bt的左子树
-        cout << bt->data << "\t"; // 访问根结点的数据域
-        InOrder(bt->rchild);      // 中序递归遍历bt的右子树
+        InOrder(bt->left);         // 中序递归遍历bt的左子树
+        cout << bt->data << "\t";  // 访问根结点的数据域
+        InOrder(bt->right);        // 中序递归遍历bt的右子树
     }
 }
 
-template <typename T> 
+template <typename T>
 void BiTree<T>::PostOrder(TreeNode<T>* bt) {
     if (bt == nullptr) {
         return;
     } else {
-        PostOrder(bt->lchid);     // 后序递归遍历bt的左子树
-        PostOrder(bt->rchild);    // 后序递归遍历bt的右子树
-        cout << bt->data << "\t"; // 访问根结点的数据域
+        PostOrder(bt->left);       // 后序递归遍历bt的左子树
+        PostOrder(bt->right);      // 后序递归遍历bt的右子树
+        cout << bt->data << "\t";  // 访问根结点的数据域
     }
 }
 
-int main() {
-    BiTree<char> tree;
-    tree.PreOrder();
-    cout << endl;
-    tree.InOrder();
-    cout << endl;
-    tree.PostOrder();
-    system("PAUSE");
-    return 0;
+template <typename T>
+void BiTree<T>::LevelOrder() {
+    if (root == nullptr) {
+        return;
+    }
+    deque<TreeNode<T>*> dq;
+    dq.push_back(root);  // 根指针入队
+    while (!dq.empty()) {
+        TreeNode<T>* node = dq.front();  // 取出队列首元素
+        cout << node->data << "\t";      // 访问当前节点数据
+        dq.pop_front();                  // 队列首元素出队
+        // 左子节点若存在，则入队
+        if (node->left != nullptr) {
+            dq.push_back(node->left);
+        }
+        // 右子节点若存在，则入队
+        if (node->right != nullptr) {
+            dq.push_back(node->right);
+        }
+    }
 }
-
-/*
-Input:
-AB#D##C##
-
-Output:
-A B D C
-B D A C
-D B C A
-*/
